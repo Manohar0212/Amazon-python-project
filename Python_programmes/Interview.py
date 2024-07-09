2793153807c4774cf4935ff7da408c8d5be15905
Manohar0212 <manoharyadav527@gmail.com>
2024-07-09 14:46:36 +0530

    pytest frame work

--- Python_programmes/Interview.py
+++ Python_programmes/Interview.py
@@ -1,6 +1,6 @@
-# All numbers should be natural numbers
-# every number should have two factors
-
+# # All numbers should be natural numbers
+# # every number should have two factors
+#
 # d={'a':3,'b':3,'c':1} print duplicate values not in a dictionary
 # dict1={'a':3,'b':3,'c':1}
 # dict2={}
@@ -9,23 +9,23 @@
 #  if value not in dict2.values():
 #             dict2[key]=value
 # print(dict2)
+# #
+# # # dictA={'a':3,'b':3,'c':1} print duplicate values in a dictionary
+# # # dictA = {'Sun': 5, 'Mon': 3, 'Tue': 5, 'Wed': 4}
+# # dictA={'a':3,'b':3,'c':1}
+# # print("Given Dictionary :", dictA)
+# # dictB = {}
+# # for key, value in dictA.items():
+# #    dictB.setdefault(value, set()).add(key)
+# # res = filter(lambda x: len(x) >1, dictB.values())
+# # Result+
+# # print("New Dictionary:",list(res))
 #
-# # dictA={'a':3,'b':3,'c':1} print duplicate values in a dictionary
-# # dictA = {'Sun': 5, 'Mon': 3, 'Tue': 5, 'Wed': 4}
-# dictA={'a':3,'b':3,'c':1}
-# print("Given Dictionary :", dictA)
-# dictB = {}
-# for key, value in dictA.items():
-#    dictB.setdefault(value, set()).add(key)
-# res = filter(lambda x: len(x) >1, dictB.values())
-# Result+
-# print("New Dictionary:",list(res))
-
-
-
-
-# l=[2,3,5,7,8,9,21,2,3,27] print prime numbers in a list
-
+#
+#
+#
+# # l=[2,3,5,7,8,9,21,2,3,27] print prime numbers in a list
+#
 # l = [2, 3, 5, 7, 8, 9, 21, 2, 3, 27]
 # prime=[]
 # # c=0
@@ -38,17 +38,17 @@
 #         prime.append(i)
 # print("prime numbers",prime)
 
-# print duplicate numbers in a list
-# l=[1,4,4,4,5,6,7,4,3,3,9]
-# u=[]
-# for num in l:
-#       if num not in u:
-#        u.append(num)
-# print(u)
-
-
-# How to remove consecutive duplicates from a list
-
+# # print duplicate numbers in a list
+# # l=[1,4,4,4,5,6,7,4,3,3,9]
+# # u=[]
+# # for num in l:
+# #       if num not in u:
+# #        u.append(num)
+# # print(u)
+#
+#
+# # How to remove consecutive duplicates from a list
+#
 # l=[1,4,4,4,5,6,7,4,3,3,9]
 # l+=[" "]
 # w=[]
@@ -58,44 +58,82 @@
 #         # print(i)
 #         w.append(l[i-1])
 # print(w)
+# #
+#
+#
+#
+#
 #
 
+#
+#
+# # l=[1,4,4,4,5,6,7,4,3,2,5,8,9,10]
+# # l+=" "
+# # w=[]
+# # for i in range(1,len(l)):
+# #     if l[i]!=l[i-1]:
+# #      w.append(l[i-1])
+# # print(w)
+#
+#
+#
+# # string="jsfgsjfhiieufjfsgfsjdffg"
+# # dict={}
+# # for i in string:
+# #     if i in dict:
+# #         continue
+# #     else:
+# #         dict[i]=string.count(i)
+# # print(dict)
+#
+# # context manager
+#
+# # class file:
+# #     def__init__(self,name:str):
+# #         self.name=name
+# #     def__enter__(self):
+# #        print(f'opening{self.name}...')
+# #        return self
+# #     def__exit__(self.exc_type,exc_value,exc_tb):
+# #         print(f,Closing{self.name}....)
+# # if__name__=='__main__':
+# #    with File('main.py') as file:
+# #        print(file.name)
+# #    print('Done!')
 
 
 
 
 
 
-# l=[1,4,4,4,5,6,7,4,3,2,5,8,9,10]
-# l+=" "
-# w=[]
-# for i in range(1,len(l)):
-#     if l[i]!=l[i-1]:
-#      w.append(l[i-1])
-# print(w)
 
 
 
-string="jsfgsjfhiieufjfsgfsjdffg"
-dict={}
-for i in string:
-    if i in dict:
-        continue
-    else:
-        dict[i]=string.count(i)
-print(dict)
 
-# context manager
 
-# class file:
-#     def__init__(self,name:str):
-#         self.name=name
-#     def__enter__(self):
-#        print(f'opening{self.name}...')
-#        return self
-#     def__exit__(self.exc_type,exc_value,exc_tb):
-#         print(f,Closing{self.name}....)
-# if__name__=='__main__':
-#    with File('main.py') as file:
-#        print(file.name)
-#    print('Done!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# a=[2,7,11,15]
+# target=9
+# for i in a:
+#  for j in range(1,i):
+#      print(i,j)
+#  print()
+        # if i+j==target:
+        #     print(i+j)
+
+
